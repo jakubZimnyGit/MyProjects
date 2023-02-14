350e2c096b351b93db77014176be414ee30c2504
jakubZimnyGit <jakub.zimny.gdansk@gmail.com>
2023-02-14 14:15:57 +0100

    Saving note as a file
    I managed to save and later load all of users subjects and notes. Still need to work on some of those variables names, for now they might be a bit confiusing. Also need to update "Delete" button, and look for some bugs in my program

--- Notebook/Notebook/Form1.cs
+++ Notebook/Notebook/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Notebook
 {
@@ -27,8 +28,41 @@ namespace Notebook
 
         private void Notebook_Load(object sender, EventArgs e)
         {
+            
             editing = false;
+            DirectoryInfo di;
+            try
+            {
+                di = new DirectoryInfo(".//Notes");
+                if(!di.Exists) 
+                {
+                    di.Create();
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            string[] files = new string[(di.GetDirectories().Length)];
+            files = converToStringArray(di.GetDirectories());
+            classNames = files.ToList();
+            classes = LoadDataTables(files);
+            saveToDirectory.Items.AddRange(files);
+            loadFromDirectory.Items.AddRange(files);
+            loadNotes();
         }
+
+        private string[] converToStringArray(DirectoryInfo[] directoryInfos)
+        {
+            string[] strings = new string[directoryInfos.Length];
+            for(int i = 0; i < directoryInfos.Length; i++)
+            {
+                strings[i] = directoryInfos[i].Name.ToString();
+            }
+            return strings;
+        }
+
         private void saveBtn_Click(object sender, EventArgs e)
         {
             SelectedClassToSaveTo = classNames.IndexOf(saveToDirectory.Text);
@@ -37,7 +71,12 @@ namespace Notebook
                 CurrentClass = classes[SelectedClassToSaveTo];
                 if (CurrentClass != null && !editing)
                 {
-                    CurrentClass.Rows.Add(tbTitle.Text, tbNote.Text);              
+                    CurrentClass.Rows.Add(tbTitle.Text, tbNote.Text);
+                    StreamWriter sw = new StreamWriter($".//Notes//{saveToDirectory.Text}//{tbTitle.Text}.txt");
+                    sw.Write(tbNote.Text);
+                    sw.Close();
+                    tbTitle.Text = "";
+                    tbNote.Text = "";
                 }
                 if (editing)
                 {
@@ -45,6 +84,7 @@ namespace Notebook
                     CurrentClass.Rows[previosNotes.CurrentCell.RowIndex]["Note"] = tbNote.Text;
                     tbTitle.Text = "";
                     tbNote.Text = "";
+                    editing = false;
                 }
                 
             }       
@@ -55,6 +95,8 @@ namespace Notebook
             if (CurrentClass != null && previosNotes.CurrentCell != null)
             {
                 CurrentClass.Rows[previosNotes.CurrentCell.RowIndex].Delete();
+                tbNote.Text = "";
+                tbTitle.Text = "";
             }
         }
 
@@ -69,11 +111,16 @@ namespace Notebook
                     classNames.Add(form2.GivenClassName);
                     DataTable newClass = NowyPrzedmiot();
                     classes.Add(newClass);
+                    DirectoryInfo di = new DirectoryInfo($".//Notes//{form2.GivenClassName}");
+                    if (!di.Exists)
+                    {
+                        di.Create();
+                    }
                 }   
             }
             
         }
-        private DataTable NowyPrzedmiot()
+        private static DataTable NowyPrzedmiot()
         {
             DataTable przedmiot = new DataTable();
             przedmiot.Columns.Add("Title");
@@ -102,5 +149,37 @@ namespace Notebook
             tbNote.Text = CurrentClass.Rows[previosNotes.CurrentCell.RowIndex].ItemArray[1].ToString();
             editing = true;
         }
+        private List<DataTable> LoadDataTables(string[] files)
+        {
+            List<DataTable> dataTable = new List<DataTable>();
+            for (int i = 0; i < files.Length; i++)
+            {
+                DataTable przedmiot = NowyPrzedmiot();
+                dataTable.Add(przedmiot);
+            }
+            return dataTable;
+        }
+        private void loadNotes()
+        {
+            DirectoryInfo di = new DirectoryInfo(".//Notes");
+            DirectoryInfo[] przedmioty = di.GetDirectories();
+            int i = 0;
+            foreach (DirectoryInfo przedmiot in przedmioty)
+            {       
+                FileInfo[] notes = przedmiot.GetFiles();
+                foreach(FileInfo note in notes)
+                {
+                                      
+                    StreamReader sr = new StreamReader(note.FullName);
+                    string title = note.Name;
+                    string tekst = sr.ReadToEnd();
+                    classes[i].Rows.Add(title,tekst);
+                    i++;
+                }
+                
+
+            }
+        }
+        
     }
 }
